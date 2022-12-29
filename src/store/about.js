f43c6454f861d56409723b5d2db81e5cf3c6f488
taeyeon <eun210818@gmail.com>
2022-12-29 14:43:02 +0900

    -

--- src/store/about.js
+++ src/store/about.js
@@ -3,7 +3,7 @@ export default {
   state: () => ({
     name: '웅',
     email: 'eun210818@gmail.com',
-    blog: 'https://www.google.com',
+    // blog: 'https://www.google.com',
     phone: '+82-10-8642-7656',
     image: 'https://heropy.blog/css/images/logo.png'
   })
